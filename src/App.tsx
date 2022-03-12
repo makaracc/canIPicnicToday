6ab9c59c259e8df66fe9345b5c5fc2091c40dc62
makaracc <makara.chhaing@gmail.com>
2022-03-12 11:30:14 +1100

    add env

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Box,
   Button,
@@ -9,6 +9,12 @@ import {
   SelectChangeEvent,
   Stack,
 } from "@mui/material";
+import axios from "axios";
+
+const Melbourne = {
+  lat: -37.840935,
+  long: 144.946457,
+};
 
 export const App: React.FC = () => {
   const [city, setCity] = React.useState("");
@@ -17,18 +23,36 @@ export const App: React.FC = () => {
     setCity(event.target.value as string);
   };
 
+  useEffect(() => {
+    console.log(JSON.stringify(import.meta.env.VITE_API));
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${
+          Melbourne.lat
+        }&lon=${Melbourne.long}&exclude=minutely&appid=${
+          import.meta.env.VITE_API
+        }`
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   const component = (
     <Box
       sx={{ display: "flex", textAlign: "center", justifyContent: "center" }}
     >
       <Stack direction="row" width="30%" gap={2}>
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <InputLabel id="demo-simple-select-label">City</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
             value={city}
-            label="Age"
+            label="City"
             onChange={handleCityChange}
           >
             <MenuItem value="Melbourne">Melbourne</MenuItem>
