698ab636ce7e9a44544a2b0b49e32f2553a885cb
makaracc <makara.chhaing@gmail.com>
2022-05-12 23:56:44 +1000

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -141,6 +141,7 @@ export const App: React.FC = () => {
   const first = (
     <Box>
       <FormControl>
+        <Button>Hi</Button>
         <InputLabel id="demo-simple-select-label">City</InputLabel>
         <Select
           labelId="demo-simple-select-label"
